2c4b55045dc6f26c9d4ef335152fec7affe800d3
Richard Sun <rsun@media.ucla.edu>
2016-04-09 23:48:42 -0700

    basic web setup

--- www/index.php
+++ www/index.php
@@ -1,1 +1,21 @@
-<h1>hello</h1>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CS143 Project 1A</title>
+</head>
+<body>
+	<?php $query = $_GET['query'] ?>
+	<form action="." method="GET">
+		<textarea cols="80" rows="10" name="query"><?php echo $query ?></textarea>
+		<br>
+		<input type="submit" value="Submit">
+	</form>
+
+	<?php
+	if ($query) {
+		echo $query;
+		echo "<table></table>";
+	}
+	?>
+</body>
+</html>
