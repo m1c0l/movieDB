946bd8f29437d8c0155621f0e0a94b8af634a071
Michael Li <michael.li11702@gmail.com>
2016-04-29 17:32:00 -0700

    Basic Bootstrap template up

---
+++ www/includes/footer.php
@@ -0,0 +1,7 @@
+		</div> <!-- container -->
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="assets/jquery-1.12.3.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	</body>
+</html>
---
+++ www/index.php
@@ -0,0 +1,9 @@
+<?php
+$TITLE="HOME";
+include 'includes/header.php';
+?>
+<h1>blah</h1>
+<button>bleh</button>
+<?php
+include 'includes/footer.php';
+?>
