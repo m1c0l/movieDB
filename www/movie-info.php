823eb5e9e4c012e276023cc1b8b1518f05523872
Michael Li <michael.li11702@gmail.com>
2016-05-01 02:53:00 -0700

    Error checking, add director and genre info

--- www/movie-info.php
+++ www/movie-info.php
@@ -6,17 +6,42 @@ if (empty($_GET['mid'])) {
 }
 else {
 	$mid = $_GET['mid'];
-	$sql = "SELECT * FROM Movie WHERE id=$mid";
-	$result = $mysqli->query($sql);
-	if (!$result || $result->num_rows == 0) {
+	$movieSql = "SELECT * FROM Movie WHERE id=$mid";
+	$movieResult = $mysqli->query($movieSql);
+	if (!$movieResult || $movieResult->num_rows == 0) {
 		echo "<p>Invalid movie ID</p>";
 	}
 	else {
-		$record = $result->fetch_assoc();
-		echo "<h3>${record['title']} (${record['year']}) information</h3>";
-		echo "<p>Directed by: <br/>";
-		echo "Produced by: ${record['company']} <br/>";
-		echo "Genre: <br/>";
+		$movieRecord = $movieResult->fetch_assoc();
+		$directorSql = "SELECT first, last FROM MovieDirector as MD, Director as D WHERE MD.mid=$mid AND MD.did=D.id";
+		$directorResult = $mysqli->query($directorSql);
+		$directorStr = "";
+		if (!$directorResult || $directorResult->num_rows == 0) {
+			$directorStr = "No director listed";
+		}
+		else {
+			$directorRecord = $directorResult->fetch_assoc();
+			$directorStr = $directorRecord['first'] . " " . $directorRecord['last'];
+		}
+		$genreSql = "SELECT genre FROM MovieGenre WHERE mid=$mid";
+		$genreResult = $mysqli->query($genreSql);
+		$genreStr = "";
+		if (!$genreResult || $genreResult->num_rows == 0) {
+			$genreStr = "No genres listed";
+		}
+		else {
+			$genreArr = array();
+			while ($genreRows = $genreResult->fetch_assoc()) {
+				$genreArr[] = $genreRows['genre'];
+			}
+			$genreStr = implode(", ", $genreArr);
+		}
+		//print_r($directorResult);
+		echo "<h3>${movieRecord['title']} (${movieRecord['year']}) information</h3>";
+		echo "<p>";
+		echo "Directed by: $directorStr<br/>";
+		echo "Produced by: ${movieRecord['company']} <br/>";
+		echo "Genre: $genreStr<br/></p>";
 	}
 }
 ?>
