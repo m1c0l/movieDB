2b2e30200025ef233577cb48dbb5835f89bb3257
Michael Li <michael.li11702@gmail.com>
2016-05-01 18:58:32 -0700

    Error checking, add genres to add movie page

--- www/add-movie.php
+++ www/add-movie.php
@@ -1,6 +1,12 @@
 <?php
 $TITLE="Add Movies";
 include 'includes/header.php';
+$genreSql = "SELECT DISTINCT genre FROM MovieGenre";
+$genreResult = $mysqli->query($genreSql);
+$genreArr = array();
+while ($genreRow = $genreResult->fetch_assoc()) {
+	$genreArr[] = $genreRow['genre'];
+}
 ?>
 <p>Add a movie.</p>
 <form action="add-movie.php" method="POST" class="form-horizontal">
@@ -35,16 +41,24 @@ include 'includes/header.php';
 			<input type="text" class="form-control" name="company" id="company" placeholder="Company" maxlength="50" required>
 		</div>
 	</div>
-
+	<div class="form-group">
+		<div class="col-sm-12">Genres</div>
+		<?php foreach($genreArr as $g) {?>
+		<div class="col-sm-2">
+			<input type="checkbox" name="genres[]" id="<?php echo $g;?>" value="<?php echo $g;?>" />
+			<label for="<?php echo $g;?>"><?php echo $g;?></label>
+		</div>
+		<?php } ?>
+	</div>
 	<button type="submit" class="btn btn-default">Submit</button>
 </form>
 
 <?php
 	if (count($_POST) > 0) {
-		$title = $_POST['title'];
-		$year = $_POST['year'];
+		$title = $mysqli->real_escape_string($_POST['title']);
+		$year = $mysqli->real_escape_string($_POST['year']);
 		$rating = $_POST['rating'];
-		$company = $_POST['company'];
+		$company = $mysqli->real_escape_string($_POST['company']);
 
 		$good_input = true;
 		$message = '';
@@ -59,9 +73,22 @@ include 'includes/header.php';
 		else {
 			$mysqli->query("UPDATE MaxMovieID SET id = id + 1");
 			$message = "Successfully added \"$title\"!";
+			if (isset($_POST['genres'])) {
+				$genresSubmitted = $_POST['genres'];
+				foreach($genresSubmitted as $g) {
+					$sql = "INSERT INTO MovieGenre VALUES($id, '$g')";
+					//echo $sql;
+					$mysqli->query($sql);
+					if ($mysqli->error) {
+						$good_input = false;
+						//echo $mysqli->error;
+					}
+				}
+			}
 		}
 		$color = $good_input ? "text-success" : "text-danger";
-		echo "<p class=\"$color\">$message</p>";
+		$escapedMsg = stripslashes($message);
+		echo "<p class=\"$color\">$escapedMsg</p>";
 	}
 ?>
 
