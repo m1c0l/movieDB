cee4bd09b22040911804b8f86b5d4d65585de9ee
Richard Sun <rsun@media.ucla.edu>
2016-05-01 19:12:37 -0700

    header

--- www/includes/header.php
+++ www/includes/header.php
@@ -33,6 +33,7 @@ $mysqli = new mysqli('localhost', 'cs143', '', 'CS143');
 				</div>
 				<div id="navbar" class="collapse navbar-collapse">
 					<ul class="nav navbar-nav">
+						<li><a href="search.php">Actors/Movies search</a></li>
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
 								Add new content<span class="caret"></span>
@@ -45,9 +46,6 @@ $mysqli = new mysqli('localhost', 'cs143', '', 'CS143');
 								<li><a href="movie-director.php">Add a director to a movie</a></li>
 							</ul>
 						</li>
-
-						<li><a href="#about">About</a></li>
-						<li><a href="search.php">Actors/Movies search</a></li>
 					</ul>
 				</div><!--/.nav-collapse -->
 			</div>
