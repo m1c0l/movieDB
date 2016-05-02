8bea6740691937afca848bd9092b276ee8edf653
Michael Li <michael.li11702@gmail.com>
2016-05-01 19:11:53 -0700

    Add more links to index

--- www/index.php
+++ www/index.php
@@ -3,8 +3,15 @@ $TITLE = "Home";
 include 'includes/header.php';
 ?>
 <h1>Welcome to MovieDB!</h1>
-<a href="search.php">Search for actors and movies</a>
-<a href="add-actor.php">Add a new actor</a>
+Try any of the following links:
+<ul>
+	<li><a href="search.php">Search for actors and movies</a></li>
+	<li><a href="add-actor.php">Add a new actor</a></li>
+	<li><a href="add-director.php">Add a new director</a></li>
+	<li><a href="add-movie.php">Add a new movie</a></li>
+	<li><a href="movie-actor.php">Add an actor to a movie</a></li>
+	<li><a href="movie-director.php">Add a director to a movie</a></li>
+</ul>
 <?php
 include 'includes/footer.php';
 ?>
